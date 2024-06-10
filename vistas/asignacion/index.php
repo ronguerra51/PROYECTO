d819f3ad038bc72d2bc5140f759446c333c97dbd
Roger <ronguerra51@gmail.com>
2024-06-10 09:12:36 -0600

    se modifico el estilo de un formulario

--- vistas/asignacion/index.php
+++ vistas/asignacion/index.php
@@ -27,46 +27,46 @@ try {
 <?php include_once '../../vistas/templates/header.php' ?>
 
 <div class="container mt-5">
-    <h1 class="text-center">ASIGNACION DE PUESTOS</h1>
+    <h1 class="text-center">ASIGNACIÓN DE PUESTOS</h1>
     <div class="row justify-content-center">
         <?php if ($error): ?>
             <div class="alert alert-danger">
                 <?= htmlspecialchars($error) ?>
             </div>
         <?php endif; ?>
-        <form action="../../controladores/asignacion/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
-            <div class="row mb-3">
+        <form action="../../controladores/asignacion/guardar.php" method="POST" class="col-lg-8 border bg-light p-4 rounded shadow">
+            <div class="row mb-4">
                 <div class="col">
-                    <label for="empleado_id">EMPLEADO</label>
-                    <select name="empleado_id" id="empleado_id" class="form-control">
-                        <option value="">SELECCIONE...</option>
+                    <label for="empleado_id" class="form-label">Empleado</label>
+                    <select name="empleado_id" id="empleado_id" class="form-select">
+                        <option value="">Seleccione...</option>
                         <?php foreach ($empleados as $empleado) : ?>
                             <option value="<?= htmlspecialchars($empleado['empleado_id']) ?>"><?= htmlspecialchars($empleado['empleado_nombre']) ?></option>
                         <?php endforeach ?>
                     </select>
                 </div>
             </div>
-            <div class="row mb-3">
+            <div class="row mb-4">
                 <div class="col">
-                    <label for="puesto_id">PUESTO</label>
-                    <select name="puesto_id" id="puesto_id" class="form-control">
-                        <option value="">SELECCIONE...</option>
+                    <label for="puesto_id" class="form-label">Puesto</label>
+                    <select name="puesto_id" id="puesto_id" class="form-select">
+                        <option value="">Seleccione...</option>
                         <?php foreach ($puestos as $puesto) : ?>
                             <option value="<?= htmlspecialchars($puesto['puesto_id']) ?>"><?= htmlspecialchars($puesto['puesto_nombre']) ?></option>
                         <?php endforeach ?>
                     </select>
                 </div>
             </div>
-            <div class="row mb-3">
+            <div class="row mb-4">
                 <div class="col">
-                    <button type="submit" class="btn btn-primary w-100">ASIGNAR</button>
+                    <button type="submit" class="btn btn-primary w-100">Asignar</button>
                 </div>
             </div>
             <div class="row">
-            <div class="col">
-                <a href="../../vistas/asignacion/buscar.php" class="btn btn-info w-100">BUSCAR</a>
+                <div class="col">
+                    <a href="../../vistas/asignacion/buscar.php" class="btn btn-info w-100">Buscar</a>
+                </div>
             </div>
-        </div>
         </form>
     </div>
 </div>
